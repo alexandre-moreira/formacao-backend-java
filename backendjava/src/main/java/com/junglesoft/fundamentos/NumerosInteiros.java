4bce67b1a170130002872c55c2b3524f4659ca27
Alexandre Moreira <4lexandre.moreira@gmail.com>
2024-05-30 19:36:00 -0300

    feat: Numeros Inteiros

---
+++ backendjava/src/main/java/com/junglesoft/fundamentos/NumerosInteiros.java
@@ -0,0 +1,25 @@
+package org.example.fundamentos;
+//02
+public class NumerosInteiros {
+    public static void main(String[] args) {
+        int idadePredro;
+        idadePredro = 30;
+
+        System.out.println("Idade do Pedro é: " + idadePredro + "anos");
+
+        int idadaAna = 23;
+        System.out.println("Idade Ana" + idadaAna);
+
+        // byte  - 8 bits  = -128 até 127
+        // short - 16 bits = -32768 até 32767
+        // int   - 32 bits
+        // long  - 64 bits
+
+        byte testaByte = 127;
+
+        int testaExpressao = 10 * 5 + 50 + 24 / 2;
+
+        System.out.println("O resultado da expressao é: " + testaExpressao);
+
+    }
+}
