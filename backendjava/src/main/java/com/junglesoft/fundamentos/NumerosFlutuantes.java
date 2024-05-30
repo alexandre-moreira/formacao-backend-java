c6a03cff70535f79593dff4315e0db21decf18ea
Alexandre Moreira <4lexandre.moreira@gmail.com>
2024-05-30 19:36:45 -0300

    feat: Numeros Flutuantes

---
+++ backendjava/src/main/java/com/junglesoft/fundamentos/NumerosFlutuantes.java
@@ -0,0 +1,17 @@
+package org.example.fundamentos;
+
+public class NumerosFlutuantes {
+    public static void main(String[] args) {
+        // float - 32 bits
+
+        // double - 64 bits
+        double nota1 = 10;
+        double nota2 = 9.5;
+        double media = (nota1 + nota2)/2;
+
+        System.out.println("Nota 1: " + nota1);
+        System.out.println("Nota 2: " + nota2);
+        System.out.println("Média: " + media);
+
+    }
+}
